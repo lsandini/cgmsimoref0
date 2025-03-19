5fee1342601d962cd2e77257e05996b7f6c9bf72
Lorenzo <lsandini@gmail.com>
2025-03-19 22:56:34 +0200

    error fixes devicestatus II

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const loop = require(`./index.js`);
 console.log(`CGM simulator started. Press Ctrl+C to exit.`);
 
 const cronLoop = cron.schedule(
-  `01,06,11,16,21,26,31,36,41,46,51,56 * * * *`,
+  `*/5 * * * *`,
   () => {
     loop();
   },
